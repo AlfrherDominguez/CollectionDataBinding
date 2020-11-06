cf529d08e512b99e94879f34e673a725450be8b5
Alfrher Emmanuel Dominguez Gonzalez <19470395@itcampeche.edu.mx>
2020-11-05 21:02:02 -0600

    Agregar Data Bindinga una coleccion de objetos

--- CollectionDataBinding/User.cs
+++ CollectionDataBinding/User.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
-namespace CollectionDataBinding
+namespace ChangeNotificationSample
 {
-    class User
+    class User : INotifyPropertyChanged
     {
+        private string name; 
+        public string Name 
+        { get => name; 
+          set 
+          { 
+                if (name != value)
+                { 
+                    name = value;
+                    OnPropertyChanged("Name");
+
+                } 
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged; 
+        private void OnPropertyChanged(string propertyName) 
+        { 
+            if (PropertyChanged != null) 
+            { PropertyChanged(this, new PropertyChangedEventArgs(propertyName)); 
+            } 
+        }
     }
 }
